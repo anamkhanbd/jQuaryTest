1efad772e63931bac9375d9837af4e9214f1320f
A H ANAM <anamkhanbd2@gmail.com>
2023-10-23 07:56:28 +0600

    use js css proparty in case protect website code

--- jQuary/jQuary.js
+++ jQuary/jQuary.js
@@ -1,3 +1,13 @@
-$document.ready() {
+$(document).ready(function() {
+    var a = $("body").html(); // Corrected the selector for the body element
+    console.log(a);
 
-}
+    $('*').css('background', 'white');
+    $('h3').css('color', 'crimson');
+    $('p:first').css({
+        'text-align': ' center',
+        'color': 'crimson',
+        'font-size': 'large',
+        'font-family': 'sans-serif'
+    });
+});
